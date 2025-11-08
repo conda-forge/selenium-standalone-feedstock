0e281a00cb283df03b88c37d87781cc8a2e30beb
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-07 22:09:15 -0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
@@ -13,6 +14,13 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/selenium-standalone << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/selenium-standalone/bin/selenium-standalone "\$@"
+EOF
+chmod +x ${PREFIX}/bin/selenium-standalone
+
 tee ${PREFIX}/bin/selenium-standalone.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\selenium-standalone %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\selenium-standalone\bin\selenium-standalone %*
 EOF
